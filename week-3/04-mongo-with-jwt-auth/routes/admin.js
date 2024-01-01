69caba6757c0adc1d688baffdf2e0ac6f358eea6
PranavKumar9529desai <dpranav7745@gmail.com>
2024-01-01 15:11:14 +0530

    cmmited the file

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,78 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { User , Admin  , Course} = require('../db');
 const router = Router();
+const jwt = require('jsonwebtoken');
+const { stringify } = require("querystring");
+// const secret = process.env.secret;
+const secret = "pranavkumar";
+console.log(secret);
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+    const username  = (req.body.username);
+    const password  = req.body.password;
+    console.log(username , password);
+
+     await Admin.create({
+        username : username ,
+        password : password
+    });
+    res.json({msg : "admin is created"});
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
-});
+    const username  = req.body.username ;
+    const password =  req.body.password;
+    const payload = {
+        username,
+    }
+    const token = jwt.sign(payload,secret);
+    await Admin.find({
+        username : username
+    }).
+    then((value)=>{
+        console.log(value);
+        if(!(value.length===0)) res.status(200).send({msg : "signIn sucessfull " , Token:token});
+        // console.log(value);
+        else{
+            res.status(400).send({msg : "failed at signin signup once again"});
+        }
+    })
+});  
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const CourseID  = req.body.CourseID ;
+    const Title = req.body.Title;
+    const Description = req.body.Description ;
+    const  Price = req.body.Price ;
+    const  Image_Url = req.body.Image_Url ;
+
+    Course.create({
+        CourseID : CourseID ,
+        Title : Title ,
+        Description : Description,
+        Price : Price ,
+        Image_Url : Image_Url
+    }).
+    then((value)=>{
+        if(!(value.length===0)) res.status(200).send({msg : "course is sucessfully created"});
+        else res.status(404).send({msg : "failed to create the couse"});
+    })
+     
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async(req, res) => {
     // Implement fetching all courses logic
+    await Course.find({})
+    .then((value)=>{
+       if(!(value.length==0)) res.status(222).send({course_availble : value});
+       else res.status(404).status({msg : "No course found"}); 
+})
 });
 
 module.exports = router;
